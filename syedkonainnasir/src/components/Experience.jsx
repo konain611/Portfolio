cd0f229f86be9972e54183518e56ce928993167d
Syed Konain Nasir <konain611@gmail.com>
2026-06-28 16:39:39 +0500

    Create Experience.jsx

---
+++ syedkonainnasir/src/components/Experience.jsx
@@ -0,0 +1,41 @@
+import Link from "next/link";
+
+export default function Experience() {
+  return (
+    <div className="flex flex-col justify-between rounded-md p-2 text-foreground">
+      <div>
+        <div className="flex items-start justify-between gap-2 pb-3">
+          <div>
+            <div className="text-sm uppercase tracking-[0.4em]"><span><i className="ri-briefcase-line text-(--accent) text-lg" /></span> Experience</div>
+          </div>
+          <button className="rounded-full border border-(--border)/40 px-3 py-1 text-xs text-foreground transition hover:border-(--border)/60 hover:text-(--accent)">
+          <Link href="/experience">
+            View All
+            </Link>
+          </button>
+        </div>
+
+        {/* <div className="grid grid-cols-3 gap-3">
+          {skills.slice(0, 9).map((skill) => (
+            <div key={skill.name} className="flex flex-col items-center gap-1 rounded-2xl text-center">
+              <div className="flex h-10 w-10 items-center justify-center rounded-2xl text-(--accent)">
+                <i className={`${skill.icon} text-xl`} />
+              </div>
+              <span className="text-xs text-foreground">{skill.name}</span>
+            </div>
+          ))}
+        </div> */}
+      </div>
+
+      {/* <div className="mt-3 rounded-2xl p-3">
+        <div className="flex items-center justify-between text-sm text-foreground">
+          <span>TypeScript</span>
+          <span>90%</span>
+        </div>
+        <div className="mt-2 h-2 overflow-hidden rounded-full bg-(--border)/20">
+          <div className="h-full rounded-full bg-(--accent)" style={{ width: '90%' }} />
+        </div>
+      </div> */}
+    </div>
+  );
+}
