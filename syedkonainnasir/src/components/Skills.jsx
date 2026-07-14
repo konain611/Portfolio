32d49baf4f8e28798536f2ea50d5cf0273cfa282
Syed Konain Nasir <konain611@gmail.com>
2026-07-14 21:14:21 +0500

    Update Skills.jsx

--- syedkonainnasir/src/components/Skills.jsx
+++ syedkonainnasir/src/components/Skills.jsx
@@ -18,18 +18,26 @@ export default function Skills() {
       <div>
         <div className="flex items-start justify-between gap-2 pb-3">
           <div>
-            <div className="text-sm uppercase tracking-[0.4em]"><span><i className="ri-code-s-slash-line text-(--accent) text-lg" /></span> Skills</div>
+            <div className="text-sm uppercase tracking-[0.4em]">
+              <span>
+                <i className="ri-code-s-slash-line text-(--accent) text-lg" />
+              </span>{" "}
+              Skills
+            </div>
           </div>
-          <button className="rounded-full border border-(--border)/40 px-3 py-1 text-xs text-foreground transition hover:border-(--border)/60 hover:text-(--accent)">
           <Link href="/skills">
-            View All
-            </Link>
-          </button>
+            <button className="rounded-full cursor-pointer border border-(--border)/40 px-3 py-1 text-xs text-foreground transition hover:border-(--border)/60 hover:text-(--accent)">
+              View All
+            </button>
+          </Link>
         </div>
 
         <div className="grid grid-cols-3 gap-2">
           {skills.slice(0, 9).map((skill) => (
-            <div key={skill.name} className="flex flex-col items-center gap-1 rounded-2xl text-center">
+            <div
+              key={skill.name}
+              className="flex flex-col items-center gap-1 rounded-2xl text-center"
+            >
               <div className="flex h-10 w-10 items-center justify-center rounded-2xl text-(--accent)">
                 <i className={`${skill.icon} text-2xl`} />
               </div>
