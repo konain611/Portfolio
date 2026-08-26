a58fb7b8e6fbb918db9b697bc3f8fb42c196dd3c
Syed Konain Nasir <konain611@gmail.com>
2026-08-26 22:20:54 +0500

    Create page.js

---
+++ syedkonainnasir/src/app/detailed/projects/page.js
@@ -0,0 +1,11 @@
+export default function ProjectsPage() {
+  return (
+      <div className="w-full max-w-[97%] mx-auto py-4 flex flex-col gap-2">
+        <div className="flex flex-col items-start border-l-4 border-(--border) py-2">
+          <h1 className="text-left text-4xl font-bold uppercase ml-4">Projects</h1>
+        </div>
+  
+      
+      </div>
+  );
+}
