e1f24b6e94911d388c9f98910f0e427c9bfb24be
Syed Konain Nasir <konain611@gmail.com>
2026-08-31 20:19:31 +0500

    Update Skills.jsx

--- syedkonainnasir/src/components/Skills.jsx
+++ syedkonainnasir/src/components/Skills.jsx
@@ -29,7 +29,7 @@ export default function Skills() {
               Skills
             </div>
           </div>
-          <Link href="/skills">
+          <Link href="/detailed/skills">
             <button className="rounded-full cursor-pointer border border-(--border)/40 px-3 py-1 text-xs text-foreground transition hover:border-(--border)/60 hover:text-(--accent)">
               View All
             </button>
