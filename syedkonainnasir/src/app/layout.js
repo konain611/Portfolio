76124680831cc03b52ef3ad0505e78b7da1b3203
Syed Konain Nasir <konain611@gmail.com>
2026-09-09 19:30:20 +0500

    Update layout.js

--- syedkonainnasir/src/app/layout.js
+++ syedkonainnasir/src/app/layout.js
@@ -1,6 +1,5 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import Footer from "@/components/Footer";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -26,7 +25,6 @@ export default function RootLayout({ children }) {
       <body className="min-h-full relative">
         <div className="h-full">
             <main className="grow">{children}
-              {/* <Footer /> */}
             </main>
         </div>
       </body>
